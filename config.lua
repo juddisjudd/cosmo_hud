8e62a1d1b2985d4c5d8fee1d8e5e6d78f7799d25
NightOwlsRP <85649796+nightowlsrp@users.noreply.github.com>
2021-08-08 13:13:52 -0700

    init

---
+++ config.lua
@@ -0,0 +1,25 @@
+Config = {}
+
+Config.AlwaysShowRadar = false -- set to true if you always want the radar to show
+Config.ShowStress = true -- set to true if you want a stress indicator
+Config.ShowSpeedo = true -- set to true if you want speedometer enabled
+Config.ShowVoice = true -- set to false if you want to hide mic indicator
+Config.UnitOfSpeed = "mph"  -- "kmh" or "mph"
+Config.UseRadio = true -- Shows headset icon instead of microphone if radio is on - REQUIRES "rp-radio"
+Config.ShowFuel = true -- Show fuel indicator
+Config.ShowBelt = true
+Config.fixedWhileBuckled = true
+Config.showUnbuckledIndicator = true
+
+Config.ejectVelocity = (40 / 2.236936)
+Config.unknownEjectVelocity = (40 / 2.236936)
+
+Config.unknownModifier = 17.0
+
+Config.playSound = true
+
+Config.volume = 0.25
+
+Config.passengerVolume = 0.20
+
+Config.playSoundForPassengers = true
